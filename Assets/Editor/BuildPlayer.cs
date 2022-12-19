0824a1235f5a21991088d5b99ceead6bcd4d93c2
MUHYUK JEON <67819713+JEONMUHYUK@users.noreply.github.com>
2022-12-19 14:09:11 +0900

    update

--- Assets/Editor/BuildPlayer.cs
+++ Assets/Editor/BuildPlayer.cs
@@ -12,8 +12,8 @@ public class BuildPlayer : MonoBehaviour
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity"};
-        //buildPlayerOptions.locationPathName = "BuildT";
-        buildPlayerOptions.locationPathName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        buildPlayerOptions.locationPathName = "onechance.exe";
+        //buildPlayerOptions.locationPathName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
 
